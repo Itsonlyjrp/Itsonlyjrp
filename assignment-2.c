e6dbcd4815f2da16c42affdf22fe397aadce10a8
Itsonlyjrp <99532313+Itsonlyjrp@users.noreply.github.com>
2022-02-12 22:00:31 +0530

    assignment-2

---
+++ assignment-2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a,b=
+	printf("enter a number");
+	scanf("%d",&a);
+	
+	printf("enter a number");
+	scanf("%d",&b);
+	
+	if(a<b)
+	printf("%d is larger\n",b);
+	else if(a>b)
+	printf("%d is larger\n",a);
+	else
+	printf("both are equal\n");
+	
+	return 0;
+}
